2ec302ed90d864f85dc0b346c0651211f91e5df1
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-06-11 16:42:48 +0500

    use memo hook the objecct is stay consistant as the same on every render
    use memo hook the objecct is stay consistant as the same on every render

--- src/AppMemo.js
+++ src/AppMemo.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { memo } from "react";
 
@@ -42,6 +42,13 @@ function App() {
     [isFakeDark]
   );
 
+  const archiveOptions = useMemo(() => {
+    return {
+      show: false,
+      title: "Post archive in addition to main Posts",
+    };
+  }, []);
+
   return (
     <section>
       <button
@@ -58,7 +65,7 @@ function App() {
         setSearchQuery={setSearchQuery}
       />
       <Main posts={searchedPosts} onAddPost={handleAddPost} />
-      <Archive show={false} />
+      <Archive archiveOptions={archiveOptions} />
       <Footer />
     </section>
   );
@@ -155,18 +162,18 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ show }) {
+const Archive = memo(function Archive({ archiveOptions }) {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
     Array.from({ length: 10000 }, () => createRandomPost())
   );
 
-  const [showArchive, setShowArchive] = useState(show);
+  const [showArchive, setShowArchive] = useState(archiveOptions.show);
 
   return (
     <aside>
-      <h2>Post archive</h2>
+      <h2>{archiveOptions.title}</h2>
       <button onClick={() => setShowArchive((s) => !s)}>
         {showArchive ? "Hide archive posts" : "Show archive posts"}
       </button>
@@ -185,7 +192,7 @@ const Archive = memo(function Archive({ show }) {
       )}
     </aside>
   );
-})
+});
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
